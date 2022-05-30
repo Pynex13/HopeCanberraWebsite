24edf9c91ceb272b5d73b402152df06efd4e3ab0
Clark Suwidjaja <clark.10698@gmail.com>
2022-05-30 23:32:42 +1000

    add things

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,3 @@ $(document).ready(function(){
   $("#mycarousel").carousel( { interval: 2000 } );
 });
 
-
-$('a[data-slide="prev"]').click(function() {
-$('#mycarousel').carousel('prev');
-});
-
-$('a[data-slide="next"]').click(function() {
-$('#mycarousel').carousel('next');
-});
